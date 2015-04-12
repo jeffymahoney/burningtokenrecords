fe8f099599ddaa5acfec91ff0feb8c1866cc2d1b
Jeff Mahoney <jeffy@jeffymahoney.com>
2015-04-12 10:22:28 -0700

    Simplifies templates.

--- docroot/sites/all/themes/btr_theme/templates/page.tpl.php
+++ docroot/sites/all/themes/btr_theme/templates/page.tpl.php
@@ -1,25 +1,9 @@
-<?php
-/**
- * @file
- * Returns the HTML for a single Drupal page.
- *
- * Complete documentation for this file is available online.
- * @see https://drupal.org/node/1728148
- */
-?>
-
 <div id="page">
-
   <header class="header" id="header" role="banner">
-
-      <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" class="header__logo-image" /></a>
-
-      <h1 class="header__site-name" id="site-name"><?php print $site_name; ?></h1>
-
+    <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" class="header__logo-image"/></a>
+    <h1 class="header__site-name" id="site-name"><?php print $site_name; ?></h1>
   </header>
-
   <div id="main">
-
     <div id="content" class="column" role="main">
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
@@ -31,5 +15,4 @@
       <?php endif; ?>
       <?php print render($page['content']); ?>
     </div>
-
-</div>
+  </div>
