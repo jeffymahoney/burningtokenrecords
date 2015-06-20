711f5820593e43a45e24c91ecd57ad77eed60088
Jeff Mahoney <jeffy@jeffymahoney.com>
2015-06-20 13:23:04 -0700

    Removes link to home page on header image when on the home page.

--- docroot/sites/all/themes/btr_theme/templates/page.tpl.php
+++ docroot/sites/all/themes/btr_theme/templates/page.tpl.php
@@ -1,6 +1,10 @@
 <div id="page">
   <header class="header" id="header" role="banner">
-    <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" class="header__logo-image"/></a>
+    <?php if ($is_front): ?>
+      <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" class="header__logo-image"/>
+    <?php else: ?>
+      <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" class="header__logo-image"/></a>
+    <?php endif; ?>
   </header>
   <div id="main-content" role="main">
     <?php print render($title_prefix); ?>
