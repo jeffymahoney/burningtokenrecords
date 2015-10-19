23aa71d8fdac5b3fc69bc2e65e14693d7c965316
Jeff Mahoney <jeffy@jeffymahoney.com>
2015-10-19 07:47:15 -0700

    Updates citations template.

--- docroot/sites/all/modules/custom/btr_citations/btr-citations.tpl.php
+++ docroot/sites/all/modules/custom/btr_citations/btr-citations.tpl.php
@@ -2,7 +2,7 @@
 /**
  * @file btr-citations.tpl.php
  * Template for BTR Citations
- * 
+ *
  * Available variables:
  * $field
  *  - contains the full render array
@@ -17,34 +17,24 @@
     foreach ($citations as $citation_title => $citation_set):
       switch ($citation_title):
         case 'External':
-          ?>
-          <h2>External links</h2>
-          <ul>
-            <?php foreach ($citation_set as $citation): ?>
-              <li>
-                <?php print $citation; ?>
-              </li>
-            <?php endforeach; ?>
-          </ul>
-
-          <?php
+          ?><h2>External links</h2><?php
+          break;
+        case 'Other':
+          ?><h2>General Pages</h2><?php
           break;
         default:
-          ?>
-          <h2><?php print $citation_title ?>s</h2>
-          <ul>
-            <?php foreach ($citation_set as $citation): ?>
-              <li>
-                <?php print $citation; ?>
-              </li>
-            <?php endforeach; ?>
-          </ul>
-          <?php
+          ?><h2><?php print $citation_title ?>s</h2><?php
           break;
-      endswitch;
-    endforeach;
-    ?>
+      endswitch; ?>
+      <ul>
+        <?php foreach ($citation_set as $citation): ?>
+          <li>
+            <?php print $citation; ?>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endforeach; ?>
   <?php else: ?>
-	<p>No Available References</p>
+    <p>No Available References</p>
   <?php endif; ?>
 </section>
